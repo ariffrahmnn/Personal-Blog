7a60dc342a6d8660668cd383f03c9eb4e1455aac
ariffrahmnn <ariffrahmnn1@gmail.com>
2026-02-05 14:16:27 +0700

    Set up first server and added get method

--- app.js
+++ app.js
@@ -1,1 +1,28 @@
-alert(`Hi, welcome to my personal blog!`)
+import express from 'express'
+import bodyParser from 'body-parser'
+import ejs from 'ejs';
+
+const app = express();
+const port = 3000;
+
+app.set("view engine", "ejs");
+
+app.use(express.static("public"));
+
+
+app.get("/", (req, res) => {
+    res.render("index.ejs")
+})
+
+app.get("/news", (req, res) => {
+    res.render("news.ejs")
+})
+
+app.get("/post", (req, res) => {
+    res.render("post.ejs")
+})
+
+
+app.listen(port, (req, res) => {
+    console.log(`App is running on port ${port}`)
+})
