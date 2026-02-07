af7d2637e542d01ea8464e680c091a6dcbc1b56b
ariffrahmnn <ariffrahmnn1@gmail.com>
2026-02-07 13:18:51 +0700

    Changed data in EJS file to be dynamic using Server

--- app.js
+++ app.js
@@ -1,19 +1,37 @@
 import express from 'express'
-import bodyParser, { urlencoded } from 'body-parser'
+import bodyParser from 'body-parser'
 import ejs from 'ejs';
 
 const app = express();
 const port = 3000;
 
-app.use(bodyParser, urlencoded({ extended:true }))
+app.use(bodyParser.urlencoded({ extended:true }))
+app.use(express.static("public"));
 
 app.set("view engine", "ejs");
 
-app.use(express.static("public"));
-
-
 app.get("/", (req, res) => {
-    res.render("index.ejs")
+    const posts = [
+        {
+            id: 1,
+            title: `My first day posting a blog`,
+            description: `i learned about Node js, and Express. This blog is my capstone project `
+        },
+
+        {
+            id: 2,
+            title: `Bootstrap is a one of styling frameworks`,
+            description: `These components in this blog is built by Bootstrap and simple CSS native. `
+        }, 
+
+        {
+            id: 3,
+            title: `Can't wait to finish this project`,
+            description: `Im always logging my progress using GitHub`
+        }
+
+    ]
+    res.render("index.ejs", { posts })
 })
 
 app.get("/news", (req, res) => {
