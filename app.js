b6f64069c8c260280201041fb92cd261c3c785ba
ariffrahmnn <ariffrahmnn1@gmail.com>
2026-02-10 16:13:52 +0700

    Refactored the code and Added new logic for post route

--- app.js
+++ app.js
@@ -6,41 +6,45 @@ const app = express();
 const port = 3000;
 
 app.use(bodyParser.urlencoded({ extended:true }))
-app.use(express.static("public"));
+app.use(express.urlencoded({ extended:true }))
 
 app.set("view engine", "ejs");
+app.use(express.static("public"));
 
+
+const posts = [];
 app.get("/", (req, res) => {
-    const posts = [
-        {
-            id: 1,
-            title: `My first day posting a blog`,
-            description: `i learned about Node js, and Express. This blog is my capstone project `
-        },
 
-        {
-            id: 2,
-            title: `Bootstrap is a one of styling frameworks`,
-            description: `These components in this blog is built by Bootstrap and simple CSS native. `
-        }, 
-
-        {
-            id: 3,
-            title: `Can't wait to finish this project`,
-            description: `Im always logging my progress using GitHub`
-        }
-
-    ]
     res.render("index.ejs", { posts })
-})
+});
 
 app.get("/news", (req, res) => {
     res.render("news.ejs")
-})
+});
 
 app.get("/post", (req, res) => {
     res.render("post.ejs")
-})
+});
+
+app.post("/create", (req, res) => {
+    const { title, description } = req.body;
+    console.log({ title, description });
+    const newPost = [
+        {
+        id: posts.length + 1,
+        title,
+        description
+     }
+
+    ];
+
+    posts.push(newPost);
+    res.redirect("/");
+});
+
+app.get("/success", (req, res) => {
+    res.render("success.ejs");
+});
 
 app.listen(port, (req, res) => {
     console.log(`App is running on port ${port}`)
