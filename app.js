2e56ff6f8a025e1bd6f72d93cbc15fbc9b02a1dd
ariffrahmnn <ariffrahmnn1@gmail.com>
2026-02-12 16:08:43 +0700

    Added new featuere that let the user post image for their thumbnail

--- app.js
+++ app.js
@@ -1,16 +1,37 @@
 import express from 'express'
 import bodyParser from 'body-parser'
 import ejs from 'ejs';
+import multer from "multer";
+import path from "path";
+import { fileURLToPath } from "url";
+import { isMainThread } from 'worker_threads';
 
 const app = express();
 const port = 3000;
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
+const storage = multer.diskStorage({
+    destination: (req, file, cb) => {
+        cb(null, path.join(__dirname, "public/uploads"));
+    },
+
+        filename: (req, file, cb) => {
+            console.log(file)
+            cb(null, Date.now()  + path.extname(file.originalname))
+    }
+})
+
+const upload = multer({storage});
+
+app.use("/uploads", express.static(path.join(__dirname, "public/uploads")));
 
 app.use(bodyParser.urlencoded({ extended:true }))
 app.use(express.urlencoded({ extended:true }))
 
 app.set("view engine", "ejs");
 app.use(express.static("public"));
-
 
 const posts = [];
 app.get("/", (req, res) => {
@@ -26,14 +47,18 @@ app.get("/post", (req, res) => {
     res.render("post.ejs")
 });
 
-app.post("/create", (req, res) => {
+app.post("/create", upload.single("image"), (req, res) => { 
+
     const { title, description } = req.body;
-    console.log({ title, description });
+
+    const image = req.file ? req.file.filename : null;
+
     const newPost =
         {
         id: posts.length + 1,
         title,
-        description
+        description,
+        image
      };
 
     posts.push(newPost);
